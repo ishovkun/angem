30e41906ee03f60687af50593c71a2375aed21bc
Igor Shovkun <igshov@gmail.com>
2020-09-14 13:18:02 -0700

    new class line segment

---
+++ src/angem/LineSegment.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "angem/Shape.hpp"
+#include "angem/Line.hpp"
+
+namespace angem {
+
+template<typename Scalar>
+class LineSegment : public Shape<Scalar>
+{
+ public:
+  LineSegment(const angem::Point<3,Scalar> & p1, const ange::Point<3,Scalar> & p2);
+  angem::Point first() const noexcept;
+  angem::Point second() const noexcept;
+};
+
+template<typename Scalar>
+LineSegment<Scalar>::LineSegment(const angem::Point<3,Scalar> & p1, const ange::Point<3,Scalar> & p2)
+{
+ this->points.push_back(p1);
+ this->points.push_back(p2);
+}
+
+template<typename Scalar>
+LineSegment<Scalar>::first() const noexcept
+{
+  assert( get_points().size() == 2 );
+  return get_points().front();
+}
+
+template<typename Scalar>
+LineSegment<Scalar>::second() const noexcept
+{
+  assert( get_points().size() == 2 );
+  return get_points().back();
+}
+
+
+}  // end namespace angem
